a362142cb4e428497b69d9981b464cdea6e47b2e
Nunuyy <amalianuy01@gmail.com>
2022-12-14 09:05:02 +0700

    fix: link mail

--- src/components/Footerv2/index.tsx
+++ src/components/Footerv2/index.tsx
@@ -75,7 +75,7 @@ export const Footerv2: React.FunctionComponent<Footerv2 & React.HTMLAttributes<H
                 </a>
               </Link>
             </div>
-
+            <div tw="text-gray-400">attorneysatlaw@malvis.co.id</div>
             <div tw="flex space-x-5 justify-center flex-wrap lg:justify-evenly w-full text-gray-400">
               {dataLinks.map((dataLinks: { to: string; title: string; offset: number }, i: number) => (
                 <Links
